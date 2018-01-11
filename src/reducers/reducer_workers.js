150b5ade4cf3040d3f3ed011ea74f6882051e214
Serhii Vyshpolskyi <svyshpol@softserveinc.com>
2018-01-11 23:05:06 +0200

    fix

--- src/reducers/reducer_workers.js
+++ src/reducers/reducer_workers.js
@@ -1,10 +1,11 @@
+import Firebase from 'firebase';
 
 const initialState = [
   {name: 'Worker1', level: 12, id: 1},
   {name: 'Worker2', level: 11, id: 2},
   {name: 'Worker3', level: 9, id: 3},
-  {name: 'Worker4', level: 10, id: 4},
- ]
+  {name: 'Worker4', level: 10, id: 4}
+ ];
 
 
 export default function(state = initialState, action) {
